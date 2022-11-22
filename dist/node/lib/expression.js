3b9407e1b74038e76af35a3777f22acece5ad0d9
David Gómez Matarrodona <david.gomez@grupoica.com>
2022-11-22 13:29:51 +0100

    fuck!

--- dist/node/lib/expression.js
+++ dist/node/lib/expression.js
@@ -79,7 +79,6 @@ function instance(token) {
 
 		var fn = '(function(path){\n\t\t\treturn function(obj,val) {\n\t\t\t\ttry {\n\t\t\t\t\t// Ensure path\n\t\t\t\t\tlet root = obj;\n\t\t\t\t\tlet kpath = path.split(\'.\');\n\t\t\t\t\tfor(let i=0; i<kpath.length;i++) {\n\t\t\t\t\t\tlet k = kpath[i];\n\t\t\t\t\t\tif(!root[k]) root[k] = {};\n\t\t\t\t\t\troot = root[k];\n\t\t\t\t\t}\n\t\n\t\t\t\t\treturn obj' + npath + ' = val;\n\t\t\t\t}catch(err) {}\n\t\t\t}\n\t\t})(\'' + path + '\')';
 
-		console.log(fn);
 		return eval(fn);
 	}
 
